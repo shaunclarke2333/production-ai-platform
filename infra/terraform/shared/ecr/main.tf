e914f67c275294b070a88f9976308a0f3e7073d9
Shaun Clarke <shaunclarke43@gmail.com>
2026-06-29 16:23:26 -0700

    feat/ecr shared repo (#5)
    * feat: added ecr repo skeleton
    
    * feat: completed and deployed ECR repo
    
    * chore: updated comments

--- infra/terraform/shared/ecr/main.tf
+++ infra/terraform/shared/ecr/main.tf
@@ -1,12 +1,12 @@
 # Creating ECR repo from local module
 module "production_ai_platform_ecr" {
-    source = "../../modules/ecr" # Path to the ECR module
+  source = "../../modules/ecr" # Path to the ECR module
 
-    ecr_repo_name = var.ecr_repo_name
-    ecr_image_tag_mutability = var.ecr_image_tag_mutability
-    ecr_encryption_type = var.ecr_encryption_type
-    ecr_image_scan_on_push = var.ecr_image_scan_on_push
-    ecr_repo_tags = var.ecr_repo_tags
+  ecr_repo_name            = var.ecr_repo_name
+  ecr_image_tag_mutability = var.ecr_image_tag_mutability
+  ecr_encryption_type      = var.ecr_encryption_type
+  ecr_image_scan_on_push   = var.ecr_image_scan_on_push
+  ecr_repo_tags            = var.ecr_repo_tags
 }
 
 # Returning the ECR repo URL as an output
